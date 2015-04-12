d9a98f4a7da8ca5c198991bd0676842ee56c70a7
frozenluo <frozenluo@gmail.com>
2015-04-13 01:03:30 +0800

    MOD: move check to my repo

---
+++ public/check.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('#keySubmit').click(function () {
+        var shortUrl = 'http://goo.gl/' + $('#keyText').val();
+        $.ajax({
+            type: "GET",
+            url: "https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyBefmtDT1cu2ypVQv-2mkF3Ux2icREUu5E&shortUrl=" + shortUrl,
+            success: function (data) {
+                var url = data.longUrl;
+                if (url.indexOf('http://www.ariesphotography.com.au/') === 0) {
+                    window.location.href = data.longUrl;
+                }
+
+            }
+        });
+    });
+});
